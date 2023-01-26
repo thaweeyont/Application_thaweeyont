9d73eb028a66109fd0041d9f307416acd551edfd
twy <webapp.website@gmail.com>
2023-01-26 11:24:30 +0700

    update flutter version

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,8 @@ import Foundation
 import firebase_core
 import firebase_messaging
 import package_info_plus
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
